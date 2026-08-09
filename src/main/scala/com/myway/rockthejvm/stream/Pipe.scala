514eee6ea32be9d418ab2b000ab657a6f6eed51d
Pascal BOSSAVIT <pascal.bossavit@gmail.com>
2026-08-09 22:42:18 +0200

    pipe

---
+++ src/main/scala/com/myway/rockthejvm/stream/Pipe.scala
@@ -0,0 +1,3 @@
+package com.myway.rockthejvm.stream
+
+object Pipe {}
---
+++ src/test/scala/com/myway/rockthejvm/stream/PipeTest.scala
@@ -0,0 +1,5 @@
+package com.myway.rockthejvm.stream
+
+class PipeTest {
+  // https://www.youtube.com/watch?v=W0jh2sO-TZ8&list=PLmtsMNDRU0ByzHzqLdoaeuKntdwCCB1d3&index=8
+}
