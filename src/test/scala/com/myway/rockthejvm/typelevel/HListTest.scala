bc20acea3a2c431f7ff0ec527bbac0b269e99b94
Pascal BOSSAVIT <pascal.bossavit@gmail.com>
2026-08-09 19:37:53 +0200

    merge sort src/main/scala/com/myway/rockthejvm/typelevel/HList.scala

--- src/test/scala/com/myway/rockthejvm/typelevel/HListTest.scala
+++ src/test/scala/com/myway/rockthejvm/typelevel/HListTest.scala
@@ -1,6 +1,6 @@
 package com.myway.rockthejvm.typelevel
 
-import com.myway.rockthejvm.typelevel.Nat.{_1, _2, _3}
+import com.myway.rockthejvm.typelevel.Nat.{_1, _2, _3, _4}
 import com.myway.rockthejvm.typelevel.TypeLevelProgramming.show
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
@@ -9,8 +9,16 @@ class HListTest extends AnyFunSuite with Matchers {
 
   test("valid split") {
 
-    val validSplit :Split[_1 :: _2 :: _3 :: HNil ,_1 :: _3 :: HNil , _2 ::  HNil ] =Split.apply
-    show(validSplit) shouldBe "TypeTag[Split[_1 :: _2 :: _3 :: HNil,_1 :: _3 :: HNil,_2 :: HNil]]"
+    val validSplit
+        : Split[_1 :: _2 :: _3 :: HNil, _1 :: _3 :: HNil, _2 :: HNil] =
+      Split.apply
+    show(
+      validSplit
+    ) shouldBe "TypeTag[Split[_1 :: _2 :: _3 :: HNil,_1 :: _3 :: HNil,_2 :: HNil]]"
+  }
+
+  test("merge") {
+    val valideMerge :Merge[_1:: _3::HNil, _2:: _4 ::HNil , _1 ::_2::_3::_4::HNil] = Merge.apply
   }
 
 }
