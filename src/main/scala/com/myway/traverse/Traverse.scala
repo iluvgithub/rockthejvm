145d94ab06c29e13da40636e83d31b53f3935478
Pascal BOSSAVIT <pascal.bossavit@gmail.com>
2026-08-20 00:13:08 +0200

    traverse

--- src/main/scala/com/myway/traverse/Traverse.scala
+++ src/main/scala/com/myway/traverse/Traverse.scala
@@ -5,7 +5,7 @@ import cats.Applicative
 trait Traverse[T[_]] {
 
   def traverse[M[_], A, B](t: T[A])(g: A => M[B])(implicit
-                                                  AP: Applicative[M]
+      AP: Applicative[M]
   ): M[T[B]]
 
   def sequence[M[_], A](t: T[M[A]])(implicit AP: Applicative[M]): M[T[A]] =
@@ -17,8 +17,8 @@ object Traverse {
 
   implicit def treeApp: Traverse[Tree] = new Traverse[Tree] {
     override def traverse[M[_], A, B](
-                                       t: Tree[A]
-                                     )(g: A => M[B])(implicit AP: Applicative[M]): M[Tree[B]] = {
+        t: Tree[A]
+    )(g: A => M[B])(implicit AP: Applicative[M]): M[Tree[B]] = {
       t.fold[M[Tree[B]]](
         x => AP.ap(AP.pure(x => Tree.tip[B](x)))(g(x)),
         l =>
@@ -35,13 +35,14 @@ object Traverse {
     }
   }
 
-
   implicit def listApp: Traverse[List] = new Traverse[List] {
 
-    override def traverse[M[_], A, B](t: List[A])(g: A => M[B])(implicit AP: Applicative[M]): M[List[B]] =
-      t.map(g).foldRight[M[List[B]]](AP.pure(Nil))(
-        (a,acc) =>
+    override def traverse[M[_], A, B](
+        t: List[A]
+    )(g: A => M[B])(implicit AP: Applicative[M]): M[List[B]] =
+      t.map(g)
+        .foldRight[M[List[B]]](AP.pure(Nil))((a, acc) =>
           AP.map(AP.product(acc, a))({ case (xs, x) => x :: xs })
-      )
+        )
   }
 }
